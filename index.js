5f73de702ddc836cce2928329478aaac12bb96a2
Mustafa <mustafabidiwala84@gmail.com>
2024-03-27 15:35:45 +0530

    preparing for deployment

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+require("dotenv").config();
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+const path = require("path");
+
+const mongoose = require("mongoose");
+const {
+  createOriginalPDF,
+  getOriginalPDF,
+  generatePDF,
+} = require("./utils/originalPDF");
+// const getOriginalPDF = require("./utils/originalPDF");
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("connected to db..."))
+  .catch((e) => console.error("Some error occured", e));
+
+app.post("/upload", upload.single("pdfFile"), function (req, res, next) {
+  console.log(req.file, req.body);
+  // // path.join(__dirname, "uploads")
+  try {
+    createOriginalPDF(req.file.filename, req.file.originalname);
+    console.log("asndjasndjns");
+    res.json({
+      docId: req.file.filename,
+      docName: req.file.originalname,
+      createdAt: "",
+    });
+    console.log(
+      "----------------------------------------------------------asndjasndjns"
+    );
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/upload/:id", (req, res) => {
+  getOriginalPDF(req.params.id)
+    .then((response) => {
+      if (response) {
+        res.json(response);
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch((e) => {});
+});
+
+app.post("/generate-pdf", async (req, res) => {
+  console.log(req.body);
+  const response = await generatePDF(
+    req.body.newName,
+    req.body.selectedPages,
+    req.body.originalPdfId
+  )
+    .then((response) => {
+      console.log("----------------------------completed", response);
+      res.send(response);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send("Internal Server error!");
+    });
+});
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.get("/message", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// app.get("/", async (req, res) => {
+//   createOriginalPDF();
+//   console.log("hi");
+// });
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}...`);
+});
---
+++ utils/originalPDF.js
@@ -0,0 +1,98 @@
+const fs = require("fs/promises");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const { PDFDocument } = require("pdf-lib");
+const originalPDF = new mongoose.Schema({
+  originalPdfId: String,
+  createdAt: { type: Date, default: Date.now },
+  originalPdfName: String,
+  newPDF: [
+    {
+      id: {
+        type: String,
+      },
+      pdfName: {
+        type: String,
+      },
+      createdAt: { type: Date },
+    },
+  ],
+});
+
+const PDF = mongoose.model("PDF", originalPDF);
+const createOriginalPDF = async (originalPdfId, originalPdfName) => {
+  let newPDF = new PDF({
+    originalPdfId,
+    originalPdfName,
+    newPDF: [],
+  });
+  newPDF
+    .save()
+    .then((pdf) => console.log(pdf))
+    .catch((e) => console.error(e));
+};
+
+const getOriginalPDF = async (originalPdfId) => {
+  const originalPDF = PDF.findOne({ originalPdfId: originalPdfId });
+  return originalPDF;
+};
+
+const generatePDF = async (newName, selectedPages, originalPdfId) => {
+  try {
+    const newPdfDoc = await PDFDocument.create();
+    console.log("1");
+    // const  = await PDFDocument.load();
+    // const srcDoc = await PDFDocument.load(
+    //   `http://localhost/uploads/${originalPdfId}`
+    // );
+    console.log(originalPdfId);
+    const pdfData = await fs.readFile(`uploads/${originalPdfId}`);
+    const srcDoc = await PDFDocument.load(pdfData);
+    // console.log(srcDoc);
+    const copiedPages = await newPdfDoc.copyPages(srcDoc, selectedPages);
+    console.log("3");
+    for (let i = 0; i < selectedPages.length; i++) {
+      newPdfDoc.addPage(copiedPages[i]);
+    }
+
+    console.log("4");
+    const pdfBytes = await newPdfDoc.save();
+    console.log("5");
+
+    try {
+      const newFileID = crypto.randomUUID();
+      await fs.writeFile(`uploads/${newFileID}.pdf`, pdfBytes);
+      console.log("File saved successfully!", newFileID);
+      const doc = await PDF.findOne({ originalPdfId });
+      console.log(
+        "-----------------------------------------------------",
+        doc.newPDF
+      );
+      doc.newPDF.push({
+        id: newFileID,
+        pdfName: newName,
+        createdAt: Date.now(),
+      });
+      console.log(
+        "-----------------------------------------------------",
+        doc.newPDF
+      );
+      await doc.save();
+      console.log(doc);
+      return newFileID;
+    } catch (e) {
+      console.error(e);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  /**
+   *
+   * SAVE TO MONGODB AND RETURN RESPONSE
+   *
+   */
+};
+
+module.exports = { createOriginalPDF, getOriginalPDF, generatePDF };
+// module.exports = getOriginalPDF;
